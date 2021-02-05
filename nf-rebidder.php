c0d420ba327db6728075e7a9c948c5a7bea30708
Matthew Heick <mjheick@users.noreply.github.com>
2021-02-05 02:47:19 -0500

    better bidding, formatting

--- nf-rebidder.php
+++ nf-rebidder.php
@@ -6,6 +6,7 @@ $config = [
 	"login_name" => "",
 	"login_password" => "",
 	"position" => 5, /* 1-12 gets you on the first page */
+	"highbid" => true,
 	"debug" => true,
 ];
 
@@ -20,9 +21,13 @@ $output = curl_exec($ch);
 
 /* See if we got redirected to log in */
 /* 301: <html><body>You are being <a href="https://www.niteflirt.com/login">redirected</a>.</body></html>*/
-if ((strpos($output, "niteflirt.com/login") !== false) && (strpos($output, "redirected") !== false)) {
+if ((strpos($output, "niteflirt.com/login") !== false) && (strpos($output, "redirected") !== false))
+{
 	/* Perform Log in. If it's successful, continue with the script */
-	if ($config["debug"]) { echo date(DATE_RFC2822) . ": logging in\n"; }
+	if ($config["debug"])
+	{
+		echo date(DATE_RFC2822) . ": logging in\n";
+	}
 	curl_setopt($ch, CURLOPT_URL, "https://www.niteflirt.com/login");
 	$output = curl_exec($ch);
 
@@ -37,7 +42,10 @@ if ((strpos($output, "niteflirt.com/login") !== false) && (strpos($output, "redi
 		"commit" => "Sign In &gt;",
 	];
 	/* Just need authenticity_token from the form */
-	if (preg_match('/<input\stype="hidden"\sname="authenticity_token"\svalue="(.*?)"\s\/>/', $output, $matches) === 1) { $form_post["authenticity_token"] = $matches[1]; }
+	if (preg_match('/<input\stype="hidden"\sname="authenticity_token"\svalue="(.*?)"\s\/>/', $output, $matches) === 1)
+	{
+		$form_post["authenticity_token"] = $matches[1];
+	}
 	$form_post["login"] = $config["login_name"];
 	$form_post["password"] = $config["login_password"];
 	/* Hook up cURL to send the log in */
@@ -52,7 +60,10 @@ if ((strpos($output, "niteflirt.com/login") !== false) && (strpos($output, "redi
 		/* Failed: We're sorry, but you've entered an incorrect email address, member name or password. Please try again. */
 		die("login failed, bad username or password");
 	}
-	if ($config["debug"]) { echo date(DATE_RFC2822) . ": login successful\n"; }
+	if ($config["debug"])
+	{
+		echo date(DATE_RFC2822) . ": login successful\n";
+	}
 }
 
 /* Set up some variables */
@@ -63,17 +74,28 @@ $balance = 0; /* How much our detected balance is */
 
 /* Parse out the page */
 /* Get the balance into $balance */
-if (preg_match('/Your\scurrent\sbalance\sis\s\$([\d\.]+)</', $output, $matches) === 1) { $balance = $matches[1]; } else { die("Balance is not detected on current page"); }
+if (preg_match('/Your\scurrent\sbalance\sis\s\$([\d\.]+)</', $output, $matches) === 1)
+{
+	$balance = $matches[1];
+}
+else
+{
+	die("Balance is not detected on current page");
+}
 
 /* Get all bids into $bid */
-if (preg_match_all('/<td\sclass="bid">(?:[\w\s()]+?)?\$([\d\.]+)<\/td>/', $output, $matches) > 0) {
+if (preg_match_all('/<td\sclass="bid">(?:[\w\s()]+?)?\$([\d\.]+)<\/td>/', $output, $matches) > 0)
+{
 	if ($config["debug"]) { echo date(DATE_RFC2822) . ": Got bid list on page 1\n"; }
 	$bids = $matches[1];
 	foreach ($matches[0] as $idx => $is_current_bid)
 	{
 		if (strpos($is_current_bid, "Current Bid") !== false)
 		{
-			if ($config["debug"]) { echo date(DATE_RFC2822) . ": I see our current bid and position\n"; }
+			if ($config["debug"])
+			{
+				echo date(DATE_RFC2822) . ": I see our current bid and position\n";
+			}
 			$my_position = $idx + 1; /* arrays are 0-based, we need exact position */
 			$current_bid = $bids[$idx]; /* If we found our bid, save it */
 			break;
@@ -81,7 +103,8 @@ if (preg_match_all('/<td\sclass="bid">(?:[\w\s()]+?)?\$([\d\.]+)<\/td>/', $outpu
 	}
 }
 
-if ($config["debug"]) {
+if ($config["debug"])
+{
 	echo date(DATE_RFC2822) . ": page details\n";
 	echo date(DATE_RFC2822) . ": \$bids: " . implode(",", $bids) . "\n";
 	echo date(DATE_RFC2822) . ": \$my_position: " . $my_position . "\n";
@@ -89,19 +112,46 @@ if ($config["debug"]) {
 	echo date(DATE_RFC2822) . ": \$balance: " . $balance . "\n";
 }
 
-/* If we are currently in our position, we're good. */
-if ($config["position"] == $my_position)
+/* If we are currently in our position or in a better position, we're good. */
+if ($my_position <= $config["position"])
 {
-	if ($config["debug"]) { echo date(DATE_RFC2822) . ": We are where we want to be\n"; }
+	if ($config["debug"])
+	{
+		echo date(DATE_RFC2822) . ": We are where we want to be\n";
+	}
 	die();
 }
 
-/* To "be" in that position we have to bid a penny more. Might want to make this random in the future */
-$bid_amt = $bids[$config["position"] - 1] + 0.01;
-if ($config["debug"]) { echo date(DATE_RFC2822) . ": For position " . $config["position"] . " we have to bid $bid_amt\n"; }
+/* If we want first, we're just gonna bid a penny more than 1st place. */
+if ($config["position"] == 1)
+{
+	$bid_amt = $bids[0] + 0.01;
+}
+else
+{
+	if ($config["highbid"])
+	{
+		/**
+		 * To "be" in that position we have to bid more. We're gonna bid 1 penny less than the next to keep our position longer.
+		 * So, for 2nd place, we need the price of 1st place (at index 0) and we subtract 1 penny from that to stick us there.
+		 * This approach is more stable and requires less bidding.
+		 */
+		$bid_amt = $bids[$config["position"] - 2] - 0.01;
+	}
+	else
+	{
+		/* At this point we butt up again next place. This approach is more volatile (like 1st place) and would require more shifts */
+		$bid_amt = $bids[$config["position"] - 1] + 0.01;
+	}
+}
+if ($config["debug"])
+{
+	echo date(DATE_RFC2822) . ": For position " . $config["position"] . " we have to bid $bid_amt\n";
+}
 
-if ($bid_amt > $balance) {
-	echo date(DATE_RFC2822) . ": We cannot set requested bid since balance < bid (\$$balance<\$$bid_amt)\n";
+if ($bid_amt > $balance)
+{
+	echo date(DATE_RFC2822) . ": We cannot set requested bid since balance < bid (\$$balance < \$$bid_amt)\n";
 	die();
 }
 
@@ -116,18 +166,30 @@ $form_post = [
 	"bid_strategy[budget]" => "",
 	"commit" => "Continue",
 ];
-if (preg_match('/<input\stype="hidden"\sname="authenticity_token"\svalue="(.*?)"\s\/>/', $output, $matches) === 1) { $form_post["authenticity_token"] = $matches[1]; }
-if (preg_match('/<input\stype="hidden"\sname="category_id"\sid="category_id"\svalue="(.*?)"\s\/>/', $output, $matches) === 1) { $form_post["category_id"] = $matches[1]; }
+if (preg_match('/<input\stype="hidden"\sname="authenticity_token"\svalue="(.*?)"\s\/>/', $output, $matches) === 1)
+{
+	$form_post["authenticity_token"] = $matches[1];
+}
+if (preg_match('/<input\stype="hidden"\sname="category_id"\sid="category_id"\svalue="(.*?)"\s\/>/', $output, $matches) === 1)
+{
+	$form_post["category_id"] = $matches[1];
+}
 
 /* Only thing that is totally special to this */
 $post_action = "";
-if (preg_match('/<form[\w\s="]+action="(.*?)"/', $output, $matches) === 1) {
+if (preg_match('/<form[\w\s="]+action="(.*?)"/', $output, $matches) === 1)
+{
 	$post_action = "https://www.niteflirt.com" . $matches[1];
-} else {
+}
+else
+{
 	die("fatal error, we cannot find POST action on bid screen.");
 }
 
-if ($config["debug"]) { echo date(DATE_RFC2822) . ": Bidding / ready to POST to $post_action\n"; }
+if ($config["debug"])
+{
+	echo date(DATE_RFC2822) . ": Bidding / ready to POST to $post_action\n";
+}
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_URL, $post_action);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($form_post));
@@ -144,8 +206,14 @@ $form_post = [
 	"confirm[confirm]" => "",
 	"commit" => "Confirm Bid",
 ];
-if (preg_match('/<input\stype="hidden"\sname="authenticity_token"\svalue="(.*?)"\s\/>/', $output, $matches) === 1) { $form_post["authenticity_token"] = $matches[1]; }
-if ($config["debug"]) { echo date(DATE_RFC2822) . ": Confirming / ready to POST to $post_action\n"; }
+if (preg_match('/<input\stype="hidden"\sname="authenticity_token"\svalue="(.*?)"\s\/>/', $output, $matches) === 1)
+{
+	$form_post["authenticity_token"] = $matches[1];
+}
+if ($config["debug"])
+{
+	echo date(DATE_RFC2822) . ": Confirming / ready to POST to $post_action\n";
+}
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_URL, $post_action);
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($form_post));
@@ -154,11 +222,22 @@ $output = curl_exec($ch);
 
 if (strpos($output, "Congratulations! You have successfully") != false)
 {
-	if ($config["debug"]) { echo date(DATE_RFC2822) . ": We are Congratulated\n"; }
-} else {
-	if ($config["debug"]) { echo date(DATE_RFC2822) . ": We are confused\n"; }
+	if ($config["debug"])
+	{
+		echo date(DATE_RFC2822) . ": We are Congratulated\n";
+	}
+}
+else
+{
+	if ($config["debug"])
+	{
+		echo date(DATE_RFC2822) . ": We are confused\n";
+	}
 }
 
 /* All done */
 curl_close($ch);
-if ($config["debug"]) { echo date(DATE_RFC2822) . ": All Done!\n"; }
+if ($config["debug"])
+{
+	echo date(DATE_RFC2822) . ": All Done!\n";
+}
